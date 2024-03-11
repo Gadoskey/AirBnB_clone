14691015a257de640e4a622d584ecd6c9bb74f2f
Opeyemi <mustopha.yusufope@gmail.com>
2024-03-11 03:27:05 -0700

    airbnb

---
+++ models/base_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Defines a class BaseModel"""
+
+    def __init__(self):
+        """Initializes a BaseModel instance"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """Updates the updated_at attribute with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary representation of the BaseModel instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
