31e5682015d9548a768fd727feea9e2b882672f1
Opeyemi <mustopha.yusufope@gmail.com>
2024-03-12 01:46:04 -0700

    dsmn

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Defines class BaseModel"""
 
 from uuid import uuid4
@@ -19,9 +20,9 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        if len(kwargs) != 0:
+        if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
+                if key == "created_at" or key == "updated_at":
                     self.__dict.__[key] = datetime.strptime(value, t_f)
                 else:
                     self.__dict.__[key] = value
