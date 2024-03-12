b5e38f960e421068dbbfd2ab156d5de0b793527a
Opeyemi <mustopha.yusufope@gmail.com>
2024-03-12 01:58:51 -0700

    oppp

--- models/base_model.py
+++ models/base_model.py
@@ -24,9 +24,9 @@ class BaseModel:
             if kwargs != "__class__":
                 for key, value in kwargs.items():
                     if key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.strptime(value, t_f))
+                        self.__dict__[key] = datetime.strptime(value, t_f)
                     else:
-                        setattr(self, key, value)
+                        self.__dict__[key] = value
 
     def save(self):
         """Updates the updated_at attribute with the current datetime"""
