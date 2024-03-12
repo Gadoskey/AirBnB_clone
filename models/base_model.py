9421ee45a13809337b9e8f1661b9afe640fc98c5
Opeyemi <mustopha.yusufope@gmail.com>
2024-03-12 01:55:15 -0700

    oppp

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,7 @@ class BaseModel:
                     if key == "created_at" or key == "updated_at":
                         setattr(self, key, datetime.strptime(value, t_f))
                     else:
-                        setatr(self, key, value)
+                        setattr(self, key, value)
 
     def save(self):
         """Updates the updated_at attribute with the current datetime"""
@@ -35,9 +35,9 @@ class BaseModel:
     def to_dict(self):
         """Returns a dictionary representation of the BaseModel instance"""
         obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
-        obj_dict["__class__"] = self.__class__.__name__
         return obj_dict
 
     def __str__(self):
